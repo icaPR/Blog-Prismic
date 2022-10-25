247f2d9362afab19980406b2a0b423bcd33fe815
Icaro Ramos <polosnramos@hotmail.com>
2022-10-25 16:58:04 -0300

    style: stylized page and post

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header>
+      <div className={styles.headerLogo}>
+        <nav>
+          <img src="/images/Logo.svg" alt="Logo" />
+        </nav>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next';
-
+import Head from 'next/head';
 import { getPrismicClient } from '../services/prismic';
+import Link from 'next/link';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   uid?: string;
@@ -24,13 +26,88 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Spacetraveling</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <div>
+            <Link href={'/post'}>
+              <h1>Como utilizar Hooks1</h1>
+            </Link>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <section>
+              <time>
+                <img src="/images/calendar.svg" alt="iconCalendar" />
+                15 mar 2021
+              </time>
+              <span>
+                <img src="/images/user.svg" alt="iconUser" />
+                Jose da Silva
+              </span>
+            </section>
+          </div>
+          <div>
+            <h1>Como utilizar Hooks2</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <section>
+              <img src="/images/calendar.svg" alt="iconCalendar" />
+              <time>15 mar 2021</time>
+              <span>
+                <img src="/images/user.svg" alt="iconUser" />
+                Jose da Silva
+              </span>
+            </section>
+          </div>
+          <div>
+            <h1>Como utilizar Hooks3</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <section>
+              <time>
+                <img src="/images/calendar.svg" alt="iconCalendar" />
+                15 mar 2021
+              </time>
+              <span>
+                <img src="/images/user.svg" alt="iconUser" />
+                Jose da Silva
+              </span>
+            </section>
+          </div>
+        </section>
+        <a href="">Carregar mais posts</a>
+      </main>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
 
-//   // TODO
-// };
+  const prismic = getPrismicClient({});
+  /* const postsResponse = await prismic.getByType();
+
+  const post = {
+    slug,
+    title: RichText.asText(postsResponse.data.title),
+    content: RichText.asHtml(postsResponse.data.content.splice(0, 3)),
+    updateAt: new Date(postsResponse.last_publication_date).toLocaleDateString(
+      'pt-BR',
+      {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }
+    ),
+  };
+
+  return {
+    props: {
+      post,
+    },
+    redirect: 60 * 30,
+  };
+*/
+};
